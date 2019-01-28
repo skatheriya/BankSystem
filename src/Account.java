91a44853d584e85cf445a5c8a39aca3ebd3c9bff
skatheiya <sachincse2@gmail.com>
2019-01-28 17:12:50 +0530

    commit first time

---
+++ src/Account.java
@@ -0,0 +1,18 @@
+
+public class Account {
+	private long accountNumber;
+	private double accountBalnace;
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getAccountBalnace() {
+		return accountBalnace;
+	}
+	public void setAccountBalnace(double accountBalnace) {
+		this.accountBalnace = accountBalnace;
+	}
+	
+}
